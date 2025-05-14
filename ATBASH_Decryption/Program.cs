b35e110ec923c231f92acb22f8b36a51766f203b
avi <a0548589558@gmail.com>
2025-05-14 16:04:28 +0300

    decrypt function added

--- ATBASH_Decryption/Program.cs
+++ ATBASH_Decryption/Program.cs
@@ -10,6 +10,44 @@ namespace ATBASH_Decryption
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(decrypt("Lfi ulixvh ziv kivkzirmt uli z nzqli zggzxp lm gsv Arlmrhg vmvnb.\r\nGsv ilxpvg fmrgh ziv ivzwb zmw dzrgrmt uli gsv hrtmzo.\r\nYlnyh szev yvvm kozxvw mvzi pvb olxzgrlmh.\r\nMfpsyz urtsgvih ziv hgzmwrmt yb uli tilfmw rmurogizgrlm.\r\nGsv zggzxp droo yv hfwwvm zmw hgilmt -- gsvb dlm’g hvv rg xlnrmt.\r\nDv nfhg hgzb srwwvm zmw pvvk gsv kozm hvxivg fmgro gsv ozhg nlnvmg.\r\nErxglib rh mvzi. Hgzb ivzwb\r\n"));
+        }
+
+
+        static char getAtBash(char c)
+        {
+            int allChars = ((int)'a') + ((int)'z');
+            int newC = allChars - (int)c;
+            return (char)newC;
+        }
+
+
+
+        static string decrypt(string str)
+        {
+            string decreptedMassege = "";
+            char bashC;
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c))
+                {
+                    bashC = getAtBash(char.ToLower(c));
+                    if (char.ToUpper(c) == c)
+                    {
+                        decreptedMassege += char.ToUpper(bashC);
+                    }
+                    else
+                    {
+                        decreptedMassege += bashC;
+                    }
+                }
+                else
+                {
+                    decreptedMassege += c;
+                }
+                
+            }
+            return decreptedMassege;
         }
     }
 }
