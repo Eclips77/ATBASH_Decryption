6ebb314172831c7030337fd7a947dda3e993db6a
Eclipse <Eclips77@users.noreply.github.com>
2025-05-14 16:02:51 +0300

    create a chacking for danger

--- ATBASH_Decryption/Program.cs
+++ ATBASH_Decryption/Program.cs
@@ -8,8 +8,28 @@ namespace ATBASH_Decryption
 {
     internal class Program
     {
+
         static void Main(string[] args)
-        {
+        {   
+            string input = Console.ReadLine();
+            Console.WriteLine(CheckForDanger(input));
         }
+
+        static int CheckForDanger(string str)
+        {
+            string[] DangersWords = { "bomb", "nukhba", "fighter", "rocket", "secret" };
+            string[] massage = str.Split(' ');
+            int pointsCounter = 0;
+            foreach (string word in massage)
+            {
+                if (DangersWords.Contains(word.ToLower()))
+                
+                    pointsCounter++;
+                
+            }
+            return pointsCounter;
+        }
+         
+                
     }
 }
